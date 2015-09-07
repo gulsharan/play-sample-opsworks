01104f1ab865a3293825ead5f6173d9c8dd9293a
Gulsharan Goraya <gulsharan@gmail.com>
2015-09-07 11:15:35 -0500

    Updated berks dependencies

---
+++ app/dependencies/play2/attributes/play2.rb
@@ -0,0 +1,5 @@
+default[:play2][:version]  = "1.3.6"
+default[:play2][:url]      = "https://downloads.typesafe.com/typesafe-activator"
+
+default[:play2][:conf]     = nil
+default[:play2][:options]  = nil
